1ddcb063708672705683357e833aaa2fabea5fcb
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-11-25 22:10:07 -0300

    feat: add 'hello world' response

--- application.rb
+++ application.rb
@@ -1,3 +1,32 @@
 # frozen_string_literal: true
 
-puts 'Hello, world!'
+require 'yaml'
+require 'json'
+require 'socket'
+
+config = YAML.load_file('config/server.yml')
+
+server = TCPServer.new(
+  config['host'],
+  config['port']
+)
+
+def send_response(client, status, body)
+  body = JSON.dump(body)
+  headers = "HTTP/1.1 #{status}\n" \
+            "Content-Type: application/json\n" \
+            "Content-Length: #{body.bytesize}\n" \
+            "Connection: close\n"
+  client.print headers
+  client.print "\n"
+  client.print body
+end
+
+loop do
+  client = server.accept
+
+  body = { message: 'Hello, world!' }
+  send_response(client, '200 OK', body)
+
+  client.close
+end
