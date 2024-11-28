f1770a80199f6c8dd8767a4f6badfc54f275632f
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-11-27 21:38:48 -0300

    refactor: use HEREDOC as multiline string

--- response.rb
+++ response.rb
@@ -8,13 +8,13 @@ class Response
   end
 
   def send(status: :ok, body: {})
-    status = get_status(status)
-    body = JSON.dump(body)
-    headers = "HTTP/1.1 #{status}\n" \
-                  "Content-Type: application/json\n" \
-                  "Content-Length: #{body.bytesize}\n" \
-                  "Connection: close\n"
-    @socket.print("#{headers}\n#{body}")
+    @socket.print <<~RESPONSE
+      HTTP/1.1 #{get_status(status)}
+      Content-Type: application/json
+
+      #{JSON.dump(body)}
+    RESPONSE
+
     @socket.close
   end
 
